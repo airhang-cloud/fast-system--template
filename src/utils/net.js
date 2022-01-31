d27d0da813fe01344d5bc57940704a5df5378034
Huangmh <huangminhang@fjzzhy.cn>
2022-01-31 12:52:48 +0800

    feat: 输出调整

--- src/utils/net.js
+++ src/utils/net.js
@@ -1,4 +1,5 @@
 const os = require("os");
+console.log(`%c 关注公众号:'部落单元阁,分享中心可点击:'http://airhang.ltd`, "color:#d8d8d8;font-size:16px;");
 //获取主机ip
 function getIPAdress() {
     var interfaces = os.networkInterfaces();
